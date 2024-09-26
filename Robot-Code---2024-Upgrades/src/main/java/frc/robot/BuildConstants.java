0590c21f8b2dca29a40d4cff1a15a3498dcd4651
Caroline Joy Pickett <cj@prodical.us>
2024-09-26 15:11:23 -0500

    Missing Files Included

--- Robot-Code---2024-Upgrades/src/main/java/frc/robot/BuildConstants.java
+++ Robot-Code---2024-Upgrades/src/main/java/frc/robot/BuildConstants.java
@@ -5,14 +5,14 @@ package frc.robot;
  */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME = "Robot-Code";
+  public static final String MAVEN_NAME = "Robot-Code---2024-Upgrades";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 179;
-  public static final String GIT_SHA = "8f7667cd9da43b35c6a382726816861a656d03fc";
-  public static final String GIT_DATE = "2024-03-15 15:24:02 CDT";
-  public static final String GIT_BRANCH = "development";
-  public static final String BUILD_DATE = "2024-03-15 16:48:36 CDT";
-  public static final long BUILD_UNIX_TIME = 1710539316863L;
+  public static final int GIT_REVISION = 21;
+  public static final String GIT_SHA = "f585b72b30718ad448beb4b84834874e3da8c835";
+  public static final String GIT_DATE = "2024-09-26 13:31:58 CDT";
+  public static final String GIT_BRANCH = "master";
+  public static final String BUILD_DATE = "2024-09-26 15:10:36 CDT";
+  public static final long BUILD_UNIX_TIME = 1727381436880L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
