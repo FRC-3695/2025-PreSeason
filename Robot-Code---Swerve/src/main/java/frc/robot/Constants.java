68665c9e624e048a557656ea485df56f3ccefdfe
Caroline Joy Pickett <cj@prodical.us>
2024-11-19 16:53:23 -0600

    Configuration built to match 24^2 frame with MK4i Swerves
    24in by 24in frame with SDS MK4i swerve modules powered by RevRobotics SparkFlex/Vortex with a CTRE Pigeon2 imu on Center.

--- Robot-Code---Swerve/src/main/java/frc/robot/Constants.java
+++ Robot-Code---Swerve/src/main/java/frc/robot/Constants.java
@@ -24,7 +24,7 @@ public final class Constants
   public static final double ROBOT_MASS = (148 - 20.3) * 0.453592; // 32lbs * kg per pound
   public static final Matter CHASSIS    = new Matter(new Translation3d(0, 0, Units.inchesToMeters(8)), ROBOT_MASS);
   public static final double LOOP_TIME  = 0.13; //s, 20ms + 110ms sprk max velocity lag
-  public static final double MAX_SPEED  = Units.feetToMeters(14.5);
+  public static final double MAX_SPEED  = Units.feetToMeters(5);
       // Maximum speed of the robot in meters per second, used to limit acceleration.
 
   public static final class AutonConstants
@@ -45,9 +45,9 @@ public final class Constants
   {
 
     // Joystick Deadband
-    public static final double LEFT_X_DEADBAND  = 0.1;
-    public static final double LEFT_Y_DEADBAND  = 0.1;
-    public static final double RIGHT_X_DEADBAND = 0.1;
+    public static final double LEFT_X_DEADBAND  = 0.2;
+    public static final double LEFT_Y_DEADBAND  = 0.2;
+    public static final double RIGHT_X_DEADBAND = 0.2;
     public static final double TURN_CONSTANT    = 6;
   }
 }
